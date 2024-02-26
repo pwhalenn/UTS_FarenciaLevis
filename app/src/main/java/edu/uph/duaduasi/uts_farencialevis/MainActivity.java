61e41bddca7db3a571152bb5124c684293dd23fb
pwhalenn <farencialevis19@gmail.com>
2024-02-26 17:58:02 +0700

    Initial commit

---
+++ app/src/main/java/edu/uph/duaduasi/uts_farencialevis/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uph.duaduasi.uts_farencialevis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
